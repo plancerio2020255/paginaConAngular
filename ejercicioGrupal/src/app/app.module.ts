0244fccaabce5f9d8881837c7d2a6637e7254de5
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-03-23 13:02:41 -0600

    Creacion de la base del proyecto

---
+++ ejercicioGrupal/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PaginaInicioComponent } from './components/pagina-inicio/pagina-inicio.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PaginaInicioComponent,
+    DashBoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
